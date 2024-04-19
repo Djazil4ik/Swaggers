7e241733a0be22f74d250abc62e553636b0f6355
Djazil <mustafaevdjaz@gmail.com>
2024-04-19 17:15:47 +0500

    First commit

---
+++ main/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    product_ref = models.TextField(unique=True)
+    product_title = models.TextField()
+    product_slug = models.TextField()
+    product_category = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
